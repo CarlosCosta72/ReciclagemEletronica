90fe5b77678be9995a9acf4b346c5b34d33b7d8c
Carlos Eduardo <carloscosta8001@gmail.com>
2025-04-10 18:18:04 -0300

    Adicionando completando

--- estoque/urls.py
+++ estoque/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('estoque/', views.ComponenteListView.as_view(), name='componente_list'),
+    path('incrementar/<int:pk>/', views.ComponenteListView.incrementar_componente, name='incrementar_componente'),
+    path('decrementar/<int:pk>/', views.ComponenteListView.decrementar_componente, name='decrementar_componente'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.db import transaction
+from django.contrib import messages
 from django.views.generic import ListView
 
 from .models import componente
 
 class ComponenteListView(ListView):
     model = componente
+    def post(self, request, *args, **kwargs):
+        with transaction.atomic():
+            nome = request.POST.get('nome')
+            quantidade = request.POST.get('quantidade')
+            descricao = request.POST.get('descricao')
+
+            componente.objects.create(nome=nome, quantidade=quantidade, descricao=descricao)
+        return redirect('componente_list')
+    def incrementar_componente(request, pk):
+        try:
+            with transaction.atomic():
+                componente_obj = get_object_or_404(componente, pk=pk)
+                componente_obj.quantidade += 1
+                componente_obj.save()
+        except Exception as e:
+            messages.error(request, f"Ocorreu um erro: {e}")
+        return redirect('componente_list')
+
+    def decrementar_componente(request, pk):
+        try:
+            with transaction.atomic():
+                componente_obj = get_object_or_404(componente, pk=pk)
+                if componente_obj.quantidade > 0:
+                    componente_obj.quantidade -= 1
+                    componente_obj.save()
+        except Exception as e:
+            messages.error(request, f"Ocorreu um erro: {e}")
+        return redirect('componente_list')
